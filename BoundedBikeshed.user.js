332081d3b4f9df7f945c1b2bf380154dad926165
Tim Kuijsten <info+git@netsend.nl>
2023-08-14 12:18:23 +0200

    release v0.2.0
    * match all but the tweakers.net frontpage
    * remove dead code and console logs
    * make userscript available

--- BoundedBikeshed.user.js
+++ BoundedBikeshed.user.js
@@ -9,7 +9,7 @@
 // @match       https://lobste.rs/s/*
 // @match       https://tweakers.net/*/*
 // @grant       none
-// @version     0.1.0
+// @version     0.2.0
 // ==/UserScript==
 
 'use strict';
