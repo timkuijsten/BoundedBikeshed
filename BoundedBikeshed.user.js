83813201b50e68ba740966e8486e6997635b8a87
Tim Kuijsten <info+git@netsend.nl>
2023-08-23 14:00:46 +0200

    release v0.4.1
    * fix first-load, set default preferences

--- BoundedBikeshed.user.js
+++ BoundedBikeshed.user.js
@@ -9,7 +9,7 @@
 // @match       https://lobste.rs/s/*
 // @match       https://tweakers.net/*/*
 // @grant       none
-// @version     0.4.0
+// @version     0.4.1
 // ==/UserScript==
 
 // Copyright (C) 2023 Tim Kuijsten
