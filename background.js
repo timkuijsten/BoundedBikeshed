f8b75e2d2f2650216c1bca447973c87f777c2df4
Tim Kuijsten <info+git@netsend.nl>
2024-01-27 22:12:44 +0100

    support "Show all comments" in Chrome

--- background.js
+++ background.js
@@ -1,15 +1,11 @@
-if (typeof browser !== "undefined") {
-  // firefox
+if (typeof browser == "undefined") {
+  globalThis.browser = chrome;
+}
+
+// Wrap in an onInstalled callback to avoid unnecessary work
+// every time the background script is run
+browser.runtime.onInstalled.addListener(() => {
   browser.action.onClicked.addListener((tab) => {
     browser.tabs.sendMessage(tab.id, { action: "toggle" });
   });
-} else if (typeof chrome !== "undefined") {
-  // chrome
-  // Wrap in an onInstalled callback to avoid unnecessary work
-  // every time the background script is run
-  chrome.runtime.onInstalled.addListener(() => {
-    chrome.action.onClicked.addListener((tab) => {
-      chrome.tabs.sendMessage(tab.id, { action: "toggle" });
-    });
-  });
-}
+});
