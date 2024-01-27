b1a48eb267f9225e7b97037dce2fd0e727b7ade4
Tim Kuijsten <info+git@netsend.nl>
2024-01-27 22:14:50 +0100

    release v0.8.0

--- BoundedBikeshed.user.js
+++ BoundedBikeshed.user.js
@@ -12,7 +12,7 @@
 // @match          https://lobste.rs/s/*
 // @match          https://tweakers.net/*/*
 // @grant          none
-// @version        0.7.1
+// @version        0.8.0
 // ==/UserScript==
 
 // Copyright (C) 2023 Tim Kuijsten
