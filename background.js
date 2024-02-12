d06aa3b4ea0a5d6631420556c0f4dc884f51b5a6
Tim Kuijsten <info+git@netsend.nl>
2024-02-13 00:17:39 +0100

    fix "Show all comments"  take 3
    It looks like the onInstalled event should have never been used.

--- background.js
+++ background.js
@@ -2,10 +2,6 @@ if (typeof browser == "undefined") {
   globalThis.browser = chrome;
 }
 
-// Wrap in an onInstalled callback to avoid unnecessary work
-// every time the background script is run
-browser.runtime.onInstalled.addListener(() => {
-  browser.action.onClicked.addListener((tab) => {
-    browser.tabs.sendMessage(tab.id, { action: "toggle" });
-  });
+browser.action.onClicked.addListener((tab) => {
+  browser.tabs.sendMessage(tab.id, { action: "toggle" });
 });
